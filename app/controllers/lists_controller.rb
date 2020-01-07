b11855c358e831db72e0c68074ccd28368d7f1fa
christophe bouquet <christophe.productowner@gmail.com>
2020-01-07 01:49:07 +0100

    all done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
 
+  before_action :set_list, only: [ :show, :edit, :update, :destroy]
+
   def index
     @lists = List.all
+    @languages = Language.all
   end
 
   def show
-    @list = List.find(params[:id])
     @flashcard = Flashcard.new
   end
 
@@ -16,27 +18,35 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to root_path
     else
       render :new
     end
   end
 
   def edit
-
   end
 
   def update
-
+    if @list.update(list_params)
+      redirect_to list_path(@list), notice: 'Your list has been successfully created'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @list.destroy
+    redirect_to root_path, notice: 'Your list was successfully destroyed'
   end
 
   private
 
   def list_params
     params.require(:list).permit(:title, :description)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
